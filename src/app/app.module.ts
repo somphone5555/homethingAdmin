a2d0cabcc32220678f6bbd806b8bbf409a9c97b5
yProgramming <yourthor.yiaveu@gmail.com>
2017-05-04 20:13:59 +0700

    Delete Spec file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import {MdButtonModule} from '@angular/material';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AddComponent } from './add/add.component';
@@ -27,7 +27,7 @@ import { QrcodeComponent } from './add/qrcode/qrcode.component';
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MdButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
